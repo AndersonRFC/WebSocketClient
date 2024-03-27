b1b6369f1496113a8e86a78213199830fb6a402a
Anderson Gomes <anderson.informatica2016@gmail.com>
2024-03-27 20:49:08 -0300

    iniciando conexão websocket, enviando e recebendo mensagens de texto

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+var button = document.getElementById("btn");
+var input = document.getElementById("input-text");
+
 console.log("ola!");
+
+console.log("tentando estabelecer conexão...");
+
+button.addEventListener("click", () => {
+  enviarTexto(input.value);
+});
+
+// --
+
+const socket = iniciarConexão();
+
+// --
+
+socket.onmessage = function(event) {
+    console.log('Mensagem do servidor:', event.data);
+};
+
+// --
+
+function iniciarConexão() {
+  const socket = new WebSocket("ws://localhost:5200/ws");
+
+  socket.onopen = (event) => {
+    console.log("Conexão estabelecida");
+  };
+
+  return socket;
+}
+
+function enviarTexto(value) {
+  socket.send(value);
+  console.log("mensagem enviada: ", value);
+}

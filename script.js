6985eb9ae7cf0c879cf842785adcbdf3c586309b
Anderson Gomes <anderson.informatica2016@gmail.com>
2024-03-27 22:23:09 -0300

    agora é possível enviar tanto mensgens quanto arquivos

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 var button = document.getElementById("btn");
+var buttonFile = document.getElementById("btn-file");
+
 var input = document.getElementById("input-text");
+var inputFile = document.getElementById("input-file");
 
 console.log("ola!");
 
 console.log("tentando estabelecer conexão...");
 
 button.addEventListener("click", () => {
-  enviarTexto(input.value);
+    enviarTexto(input.value);
+});
+
+input.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+        enviarTexto(input.value);
+    }
+});
+
+buttonFile.addEventListener("click", () => {
+    const file = inputFile.files[0];
+    if (file) {
+        enviarArquivo(file);
+    } else {
+        console.log("Nenhum arquivo selecionado.");
+    }
 });
 
 // --
@@ -15,23 +33,58 @@ const socket = iniciarConexão();
 
 // --
 
-socket.onmessage = function(event) {
-    console.log('Mensagem do servidor:', event.data);
+socket.onmessage = function (event) {
+    console.log("Mensagem do servidor:", event.data);
 };
 
 // --
 
 function iniciarConexão() {
-  const socket = new WebSocket("ws://localhost:5200/ws");
+    const socket = new WebSocket("ws://localhost:5200/ws");
 
-  socket.onopen = (event) => {
-    console.log("Conexão estabelecida");
-  };
+    socket.onopen = (event) => {
+        console.log("Conexão estabelecida");
+    };
 
-  return socket;
+    return socket;
 }
 
 function enviarTexto(value) {
-  socket.send(value);
-  console.log("mensagem enviada: ", value);
+    const message = {
+        type: "text",
+        name: "Você",
+        data: value,
+    };
+
+    const mensagemAEnviar = JSON.stringify(message);
+
+    socket.send(mensagemAEnviar);
+    console.log("mensagem enviada: ", mensagemAEnviar);
+    input.value = "";
 }
+
+function enviarArquivo(file) {
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+        const data = event.target.result.split(",")[1];
+
+        const message = {
+            type: "file",
+            name: file.name,
+            data: data,
+        };
+
+        const mensagemAEnviar = JSON.stringify(message);
+
+        socket.send(mensagemAEnviar);
+
+        console.log("Arquivo enviado: ", file.name);
+    };
+
+    reader.readAsDataURL(file);
+}
+
+/*
+
+*/
